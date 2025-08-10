e904af125f6f63a0e48b624246316bad86e24e41
sobabo <kingcrimzon05097878@gmail.com>
2025-08-10 14:45:57 +0900

    update: status

--- MeshCutSample/Assets/Source/ObjectStatus.cs
+++ MeshCutSample/Assets/Source/ObjectStatus.cs
@@ -5,6 +5,30 @@ using UnityEngine;
 /// </summary>
 public class ObjectStatus : MonoBehaviour {
 
-    [SerializeField, Tooltip("切断可能オブジェクト")]
-	private bool _isCuttable = false;
+	[SerializeField, Tooltip("ゲームアクション対象オブジェクト")]
+	private bool _isGamingObject = true;
+
+	[SerializeField, Tooltip("切断可能オブジェクト")]
+	private bool _isCuttable = true;
+
+	/// <summary>
+	/// 切断可能回数の制限
+	/// </summary>
+	private int _statusOfCutableLimit = 5;
+
+	public void UpdateCutStatus() {
+		if (!_isCuttable) {
+			return;
+		}
+		else {
+			_statusOfCutableLimit--;
+			if (_statusOfCutableLimit <= 0) {
+				_isCuttable = false;
+				Debug.Log("This object is no longer cuttable.");
+			}
+			else {
+				Debug.Log($"Remaining cuts: {_statusOfCutableLimit}");
+			}
+		}
+	}
 }
