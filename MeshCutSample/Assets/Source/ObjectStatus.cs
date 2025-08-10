bf6cc38bebddf5c88287a045ee5b1f03b28d75e2
sobabo <kingcrimzon05097878@gmail.com>
2025-08-10 14:28:31 +0900

    chore: add monobehaviour

--- MeshCutSample/Assets/Source/ObjectStatus.cs
+++ MeshCutSample/Assets/Source/ObjectStatus.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 /// <summary>
 /// オブジェクトの状態を管理するクラス
 /// </summary>
-public class ObjectStatus {
+public class ObjectStatus : MonoBehaviour {
 
     [SerializeField, Tooltip("切断可能オブジェクト")]
 	private bool _isCuttable = false;
